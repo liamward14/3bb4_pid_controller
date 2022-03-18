5eda90a0af6598634116be9359047c1cfa8c3bbe
Liam Ward <44728707+liamward14@users.noreply.github.com>
2022-03-18 11:00:25 -0400

    comment update

--- comms.h
+++ comms.h
@@ -10,6 +10,5 @@ void send_ADC10_samples(unsigned short* buff, unsigned char samples);
 void pc(unsigned char c);
 char gc(void);
 
-//TODO: set up interrupt for receiving new Kp,Ki,Kd values
 
 #endif
